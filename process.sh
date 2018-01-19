90ccb0d7e5832407a4f0cc1d0c0a2f57a938066f
D1vy4n5hu 5hukl4 <divyanshushukl@gmail.com>
2018-01-19 19:13:17 +0530

    Add files via upload
    Version:1.0
    Just for fun.

---
+++ process.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+cat << EOF
+ ____                                                         _             
+|  _ \ _ __ ___   ___ ___  ___ ___    _____  _____  ___ _   _| |_ ___  _ __ 
+| |_) | '__/ _ \ / __/ _ \/ __/ __|  / _ \ \/ / _ \/ __| | | | __/ _ \| '__|
+|  __/| | | (_) | (_|  __/\__ \__ \ |  __/>  <  __/ (__| |_| | || (_) | |   
+|_|   |_|  \___/ \___\___||___/___/  \___/_/\_\___|\___|\__,_|\__\___/|_|   
+By: ju57m0rph3u5  version 1.0                                                                                                        
+
+
+
+Select functionality:
+1)List all processes.
+2)Kill process by supplying search string.
+
+EOF
+
+read -ep "Select task to be performed [1/2]:  " ab
+
+procc=$(find /proc -mindepth 2 -maxdepth 3 -name exe -exec ls -lh {} \; 2>/dev/null |column|awk 'BEGIN {print "=======================================================\nUser                Date     Time    PID     Process\n=======================================================\n"             ;} match($9,/\/proc\/([[:digit:]]+)\/.*/,a){print $3,"  ",$6,$7," ",$8," ",a[1]," ",shellvar=$11}')
+
+while true; do
+
+
+case $ab in
+            1)
+                echo -e "\e[1;32m$procc"
+                break;;
+            2) 
+                read -ep "Enter name of the process:  " bc
+                kill=`echo "$procc" | grep -i $bc | awk '{print $5}'| xargs`
+                if [ -z "$kill" ]; then
+                    echo -e "\e[1;31mNo such process running";
+                    
+                else
+                
+                    for i in $bc; do kill -9 $kill; done
+                    echo -e "\e[1;36mProcesses Killed: " $kill
+                    
+                fi
+                break;;
+        esac
+    done
+trap conrol_c SIGINT
+
+
+
+
+
+
